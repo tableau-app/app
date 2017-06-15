16c57edae9de1fa63f5e0405605edfa3002c1495
Morgan Fogarty <mofo37@gmail.com>
2017-06-14 23:45:31 -0700

    style signin/signup

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,11 +4,11 @@ import styled from 'styled-components';
 
 const Div = styled.div`
   width: 400px;
-  height: 600px;
-  margin: 0 auto;
-  border: 1px solid #222;
+  margin: 1.5em auto;
+  border: 1px solid #999;
   border-radius: 5px;
   background-color: #eee;
+  padding: 2em 0;
 `;
 
 
