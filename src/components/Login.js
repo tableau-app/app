ae70e01154dba5040ccbfc14fc17683eedb7e732
nicky evers <livelyevers@gmail.com>
2017-06-15 16:39:47 -0700

    styled home page with flexbox

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,32 +2,32 @@ import React from 'react';
 import Auth from './Auth';
 import styled from 'styled-components';
 
-const Div = styled.div`
-  width: 48%;
-  margin: 4em 1.4em 0 0;
+const AuthDiv = styled.div`
+  min-width: 25%;
   border: 1px solid #999;
   border-radius: 5px;
   background-color: #eee;
-  padding: 2em 0;
-  float: left;
-  display: inline-block;
 `;
 
 const ImgDiv = styled.div`
-  width: 48%;
-  display: inline-block;
-  float: left;
-  padding: 4em 0 0 1.5em;
+  width: 45%;
+`;
+
+const Wrapper = styled.div`
+  margin-top: 30px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 `;
 
 
 export const Login = () => (
-  <div>
+  <Wrapper >
     <ImgDiv>
       <img src="https://s3-us-west-2.amazonaws.com/tableau-users-images/laptop-landingimg.png" alt='laptop' style={{ width: '90%'}} />
     </ImgDiv>
-    <Div>
+    <AuthDiv>
       <Auth />
-    </Div>
-  </div>
+    </AuthDiv>
+  </Wrapper>
 );
