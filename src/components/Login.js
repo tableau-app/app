0ed3a69b12b3fe6f22a5a0065ec2f91eca0a6b4b
Morgan Fogarty <mofo37@gmail.com>
2017-06-14 17:25:13 -0700

    minor style on landing page

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,15 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import OAuth from './OAuth';
 import Auth from './Auth';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  width: 400px;
+  height: 600px;
+  margin: 0 auto;
+  border: 1px solid #222;
+  border-radius: 5px;
+  background-color: #eee;
+`;
 
 
 export const Login = () => (
-  <div>
+  <Div>
     <div>
       <OAuth/>
     </div>
     <div>
       <Auth/>
     </div>
-  </div>
+  </Div>
 );
