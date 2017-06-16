bf949a8cf4832237c6bef1c0534e1d3bfb97122c
nicky evers <livelyevers@gmail.com>
2017-06-16 09:49:45 -0700

    login page button styling and font change

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
-import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 import Credentials from './Credentials';
 import { signup, signin } from '../actions';
 
@@ -15,17 +15,22 @@ const P = styled.p`
   margin: .5em 0;
 `;
 
-const H1 = styled.h1`
-  width: 75%;
+const Heading = styled.h1`
   margin: 0 auto;
-  font-size: 2em;
-  font-weight: bold;
+  font-size: 3em;
   text-align: center;
+  // font-family: 'Yesteryear', cursive;
+  font-family: 'Petit Formal Script', cursive;
 `;
 
 const Hr = styled.hr`
-  margin: 1.5em auto;
-  border: 1px solid lightgrey;
+  margin: 2em auto;
+  border: 0; 
+  height: 1px; 
+  background-image: -webkit-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0);
+  background-image: -moz-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0);
+  background-image: -ms-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0);
+  background-image: -o-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0); 
 `;
 
 const Error = styled.pre`
@@ -33,10 +38,7 @@ const Error = styled.pre`
 `;
 
 const ButtonStyle = {
-  display: 'flex',
-  justifyContent: 'center',
-  margin: '10% 25%',
-  backgroundColor: 'grey500'//not working?
+  display: 'inline-block'
 };
 
 class Auth extends Component {
@@ -55,15 +57,17 @@ class Auth extends Component {
 
     return (
       <div>
-        <H1>tableau</H1>
+        <Heading>Tableau</Heading>
         {this.state.signupForm
           ?
           <div>
             <Credentials callToAction="Sign up" submit={signup} />
             <Div>
               <Hr />
-              <P>Already have an account?</P>
-              <RaisedButton style={ButtonStyle} onClick={() => this.setState({ signupForm: false })}>Sign In</RaisedButton>
+              <P>Already have an account? 
+                <FlatButton style={ButtonStyle} primary={true} onClick={() => this.setState({ signupForm: false })}>Sign In</FlatButton>
+              </P>
+           
             </Div>
           </div>
           :
@@ -71,8 +75,10 @@ class Auth extends Component {
             <Credentials callToAction="Sign in" submit={signin} />
             <Div>
               <Hr />
-              <P>Not yet registered?</P>
-              <RaisedButton style={ButtonStyle} onClick={() => this.setState({ signupForm: true })}>Sign Up</RaisedButton>
+              <P>Not yet registered?
+                <FlatButton style={ButtonStyle} primary={true} onClick={() => this.setState({ signupForm: true })}>Sign Up</FlatButton>
+              </P>
+              
             </Div>
           </div>
         }
--- src/components/Credentials.js
+++ src/components/Credentials.js
@@ -2,16 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import RaisedButton from 'material-ui/RaisedButton';
 
-const Label = styled.label`
-  display: block;
-  margin: 1em auto .25em auto;
-  width: 75%;
-`;
-
 const Input = styled.input`
+  margin: 35px auto;
   display: block;
-  width: 75%;
-  margin: 0 auto;
+  width: 60%;
+  padding: 10px;
   font-size: 1em;
 `;
 
@@ -20,17 +15,6 @@ const ButtonStyle = {
   justifyContent: 'center',
   margin: '10% 30%'
 };
-
-// const ButtonStyle = styled.button`
-//   // background-color: #fff;
-//   // display: block;
-//   // width: 75%;
-//   // margin: 1em auto;
-//   // padding-top: 1em;
-//   // padding-bottom: 1em;
-//   // font-size: 1em;
-//   // border-radius: 10px;
-// `;
 
 export default function Credentials({ submit, callToAction }) {
   return (
@@ -46,10 +30,8 @@ export default function Credentials({ submit, callToAction }) {
         submit(data);
         e.target.reset();
       }}>
-        <Label>Username</Label>
-        <Input type="text" name="username" />
-        <Label>Password</Label>
-        <Input type="password" name="password" />
+        <Input placeholder='username' name="username" />
+        <Input placeholder='password' type="password" name="password" />
         <RaisedButton type="submit" style={ButtonStyle}>{callToAction} </RaisedButton>
       </form>
     </div>
